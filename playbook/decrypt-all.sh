a963442a51a5d2192a02c25d9ca654e581d8bd9f
Boris Ning <fartbagxp@gmail.com>
2018-06-20 20:42:26 -0400

    Adding a configuration sample

--- playbook/decrypt-all.sh
+++ playbook/decrypt-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=$(find vault/ -type f -name "*.yml")
+FILES=$(find vault/ -type f -iname "*.yml*" ! -iname  "*sample.yml")
 for line in $FILES; do 
   ansible-vault decrypt --vault-password-file ${PWD}/.vault-pass.txt "$line"
 done
